c67d6a71a246a28f418d853a197e6554f9c63523
MrPrinceSaxena <princesaxena511@gmail.com>
2026-04-30 09:42:57 +0530

    final backend dependencies

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 const express = require("express");
-const sqlite3 = require("sqlite3").verbose();
 const cors = require("cors");
+const sqlite3 = require("sqlite3").verbose();
 const bodyParser = require("body-parser");
 
 const app = express();
